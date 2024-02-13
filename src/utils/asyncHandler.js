d906160256364ff4a28dd73aba68b6855721719d
dipu-kr <dipum906@gmail.com>
2024-02-13 19:38:28 +0530

    config middlewares

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,20 @@
+//FIRST METHOD
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+// SECOND METHOD
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (err) {
+//     res.status(500).json({
+//       success: false,
+//       message: err.message,
+//     });
+//   }
+// };
+
+export { asyncHandler };
